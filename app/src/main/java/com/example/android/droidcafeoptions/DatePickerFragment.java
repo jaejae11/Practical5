aea3171aa3ce96e42ce7fd6b951e9f57a3532c35
jaejae11 <looijt-wm19@student.tarc.edu.my>
2020-07-17 19:13:59 +0800

    commit by Jia Toong

---
+++ app/src/main/java/com/example/android/droidcafeoptions/DatePickerFragment.java
@@ -0,0 +1,38 @@
+package com.example.android.droidcafeoptions;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DatePickerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(),this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        OrderActivity activity = (OrderActivity) getActivity();
+        activity.processDatePickerResult(year,month,day);
+    }
+}
